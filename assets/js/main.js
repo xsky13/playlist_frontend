dac41e0f49ca1e476f16d4bfb9cbde59698d3092
Jared Peter <aljapeter05@gmail.com>
2026-07-01 09:38:34 -0300

    experimenting with percentage ui

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,6 +84,31 @@ const addItemServer = async (video) => {
     .catch(_ => alert("Error getting video info"), null);
     if (!result) return;
 
+    // ui stuff
+    hideGlobalSpinner();
+    hideOverlay();
+    document.getElementById("progress").textContent = "";
+
+    document.querySelector(".playlist").innerHTML += `
+        <div class="playlist-item" id="${result.id}">
+            <div class="playlist-start">
+                <img src="./assets/images/play.png" style="cursor: pointer;" onclick="javascript:openSong('${result.id}')" width="50" />
+                <div class="details">
+                    <h3>${escapeHtml(result.title)}</h3>
+                    <span class="subtitle" id="${result.id}-subtitle"></span>
+                </div>
+            </div>
+            <div>
+                <span class="subtitle">${result.duration}</span>
+                <div class="delete-icon" onclick="javascript:deleteSong('${result.id}')">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width: 1.2rem; height: 1.2rem">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                    </svg>
+                </div>
+            </div>
+        </div>
+    `;
+
     const response = await fetch(`https://playlist-backend-a18f.onrender.com/extract/${result.id}`).catch(_ => alert("Error downloading file"), null);
     if (!response) return;
 
@@ -101,15 +126,13 @@ const addItemServer = async (video) => {
         received += value.length;
 
         const percent = total ? Math.min(100, Math.round(received / total * 100)) : null;
-        document.getElementById("progress").textContent = percent + "%";
+        document.getElementById(`${result.id}-subtitle`).textContent = percent + "%";
+        // document.getElementById("progress").textContent = percent + "%";
     }
 
-    const blob = new Blob(chunks, { type: "audio/mpeg" });
+    document.getElementById(`${result.id}-subtitle`).textContent = `${(received / 1024 / 1024).toFixed(1)}MB`;
 
-    // ui stuff
-    hideGlobalSpinner();
-    hideOverlay();
-    document.getElementById("progress").textContent = "";
+    const blob = new Blob(chunks, { type: "audio/mpeg" });
 
     const db = await openDatabase();
     const song = {
@@ -127,26 +150,6 @@ const addItemServer = async (video) => {
     songStore.put(song);
     const fileStore = tx.objectStore('file');
     fileStore.put(file);
-
-    document.querySelector(".playlist").innerHTML += `
-        <div class="playlist-item" id="${result.id}">
-            <div class="playlist-start">
-                <img src="./assets/images/play.png" style="cursor: pointer;" onclick="javascript:openSong('${result.id}')" width="50" />
-                <div class="details">
-                    <h3>${escapeHtml(result.title)}</h3>
-                    <span class="subtitle">${(received / 1024 / 1024).toFixed(1)}MB</span>
-                </div>
-            </div>
-            <div>
-                <span class="subtitle">${result.duration}</span>
-                <div class="delete-icon" onclick="javascript:deleteSong('${result.id}')">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width: 1.2rem; height: 1.2rem">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                    </svg>
-                </div>
-            </div>
-        </div>
-    `;
 }
 
 
